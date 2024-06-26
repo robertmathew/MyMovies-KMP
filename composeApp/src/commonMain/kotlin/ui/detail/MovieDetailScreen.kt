955c763cd60eb720a0240b4465ec9e57bb6c66f2
Robert Mathew <robert.c.mathew@gmail.com>
2024-06-26 16:13:43 +0530

    Update MovieDetailScreen layout and upgrade dependencies
    The layout in MovieDetailScreen has been revamped to use LazyColumn and Row for better arrangement and presentation of movie details. Also, dependencies in libs.versions.toml have been updated, including the Compose plugin, Kamel, and Kermit to their newer versions.

--- composeApp/src/commonMain/kotlin/ui/detail/MovieDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/detail/MovieDetailScreen.kt
@@ -3,8 +3,10 @@ package ui.detail
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,9 +20,9 @@ import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.model.rememberNavigatorScreenModel
 import cafe.adriel.voyager.core.screen.Screen
@@ -90,24 +92,75 @@ private fun MovieDetail(
     val uiState by viewModel.uiState.collectAsState()
 
     AnimatedVisibility(uiState.movieDetail != null) {
-        Column(modifier = modifier) {
-            KamelImage(
-                asyncPainterResource(data = Constant.w500ImageUrl + uiState.movieDetail?.backdropPath),
-                contentDescription = uiState.movieDetail?.title,
-                modifier = Modifier.height(200.dp),
-                contentScale = ContentScale.Crop,
-            )
-            Text(
-                text = stringResource(Res.string.description),
-                style = MaterialTheme.typography.labelMedium,
-                color = MaterialTheme.colorScheme.primary,
-                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
-            )
-            uiState.movieDetail?.overview?.let {
-                Text(
-                    it,
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+        LazyColumn(modifier = modifier) {
+            item {
+                KamelImage(
+                    asyncPainterResource(data = Constant.w500ImageUrl + uiState.movieDetail?.backdropPath),
+                    contentDescription = uiState.movieDetail?.title,
+                    modifier = Modifier.height(200.dp),
+                    contentScale = ContentScale.Crop,
                 )
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    KamelImage(
+                        asyncPainterResource(data = Constant.w500ImageUrl + uiState.movieDetail?.posterPath),
+                        contentDescription = uiState.movieDetail?.title,
+                        modifier = Modifier.weight(weight = 1f, fill = true)
+                    )
+                    Column(
+                        modifier = Modifier.weight(weight = 2f, fill = true).align(Alignment.Top),
+                    ) {
+                        Text(
+                            text = "Rating",
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                        )
+                        Text(
+                            text = uiState.movieDetail?.voteAverage.toString() + "/10",
+                            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+                        )
+                        uiState.movieDetail?.releaseDate?.let {
+                            Text(
+                                text = "Release Date",
+                                style = MaterialTheme.typography.labelMedium,
+                                color = MaterialTheme.colorScheme.primary,
+                                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                            )
+                            Text(
+                                text = it,
+                                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+                            )
+                        }
+                        uiState.movieDetail?.runtime?.toString()?.let {
+                            Text(
+                                text = "Runtime",
+                                style = MaterialTheme.typography.labelMedium,
+                                color = MaterialTheme.colorScheme.primary,
+                                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                            )
+                            Text(
+                                text = it + " mins",
+                                modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+                            )
+                        }
+                    }
+                }
+
+                Text(
+                    text = stringResource(Res.string.description),
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                )
+                uiState.movieDetail?.overview?.let {
+                    Text(
+                        it,
+                        modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)
+                    )
+                }
             }
         }
     }
